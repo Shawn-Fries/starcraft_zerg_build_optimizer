a562febe0266af62890f1ff62071e2b1e6cdb3e9
Shawn-Fries <samfoley275@gmail.com>
2022-02-10 08:20:35 -0500

    testing l ayout changes

--- src/game.js
+++ src/game.js
@@ -5,44 +5,82 @@ export default class Game {
         this.minutes = 0;
         this.drone_count = 12;
         this.minerals = 50;
-        this.mining_rate = 41.25 / 60; // Per second
+        this.mining_rate = 42 / 60; // Per second
+        this.reducedMiningRate = 18 / 60; // Per second
         this.hatchery_count = 1;
         this.drone_construction_time = 12;
         this.drone_mineral_cost = 50;
         this.hatchery_construction_time = 71;
+        this.workerTravelTime = 15;
         this.hatchery_mineral_cost = 300;
         this.max_supply = 14;
+        this.maxPossibleSupply = 200;
         this.overlord_construction_time = 18;
         this.overlord_mineral_cost = 100;
         this.hatchery_supply_increase = 6;
         this.overlord_supply_increase = 8;
+        this.currentLarvaCount = 3;
+        this.maxLarvaCount = 3;
+        this.larvaConstructionTime = 11;
     }
 
     displayTime() {
-        return `${this.minutes}:${this.seconds}`
+        let minutes = this.minutes;
+        if (minutes < 10) minutes = `0${minutes}`;
+        let seconds = this.seconds;
+        if (seconds < 10) seconds = `0${seconds}`;
+        return `${minutes}:${seconds}`
     }
 
     addTime(seconds) {
-        new_seconds = this.seconds + seconds;
+        let new_seconds = this.seconds + seconds;
         this.minutes += Math.floor(new_seconds / 60);
         this.seconds = new_seconds % 60;
     }
 
     addDrone() {
         this.addTime(this.drone_construction_time);
-        this.minerals -= this.drone_mineral_cost;
-        this.minerals += this.drone_count * this.mining_rate * this.drone_construction_time;
+        if (this.minerals >= this.drone_mineral_cost && this.currentLarvaCount > 0) {
+            this.minerals -= this.drone_mineral_cost;
+            let optimalMinerals = Math.min(this.drone_count, 16 * this.hatchery_count) * this.mining_rate * this.drone_construction_time;
+            let suboptimalMinerals = 0;
+            if (this.drone_count >= this.hatchery_count * 16) {
+                suboptimalMinerals = Math.min(this.drone_count - this.hatchery_count * 16, 8 * this.hatchery_count) * this.reducedMiningRate * this.drone_construction_time;
+            }
+            this.minerals += optimalMinerals + suboptimalMinerals;
+            this.drone_count += 1;
+           // debugger;
+        } else {
+            let optimalMinerals = Math.min(this.drone_count, 16 * this.hatchery_count) * this.mining_rate * this.drone_construction_time;
+            let suboptimalMinerals = 0;
+            if (this.drone_count >= this.hatchery_count * 16) {
+                suboptimalMinerals = Math.min(this.drone_count - this.hatchery_count * 16, 8 * this.hatchery_count) * this.reducedMiningRate * this.drone_construction_time;
+            }
+            this.minerals += optimalMinerals + suboptimalMinerals;
+            this.currentLarvaCount = Math.min(this.currentLarvaCount + 1, this.maxLarvaCount);
+        }
 
-        this.drone_count += 1;
     }
 
     addHatchery() {
-        this.addTime(this.hatchery_construction_time);
-        this.minerals -= 300;
-        this.drone_count -= 1;
-        this.max_supply += this.hatchery_supply_increase;
+        this.addTime(this.hatchery_construction_time + this.workerTravelTime);
+        if (this.minerals >= this.hatchery_mineral_cost) {
+            this.minerals -= 300;
+            this.drone_count -= 1;
+            this.hatchery_count += 1;
+            this.max_supply += this.hatchery_supply_increase;
+            this.maxLarvaCount += 3;
+            this.currentLarvaCount = Math.min(this.currentLarvaCount + 7, this.maxLarvaCount);
+        } else {
+            this.currentLarvaCount = Math.min(this.currentLarvaCount + 6, this.maxLarvaCount);
+        }
 
-        this.minerals += this.hatchery_construction_time * this.drone_count * this.mining_rate;
+        let optimalMinerals = Math.min(this.drone_count, 16 * this.hatchery_count) * this.mining_rate * (this.workerTravelTime + this.hatchery_construction_time);
+        let suboptimalMinerals = 0;
+        if (this.drone_count >= this.hatchery_count * 16) {
+            suboptimalMinerals = Math.min(this.drone_count % (this.hatchery_count * 16), 8 * this.hatchery_count) * this.reducedMiningRate * (this.workerTravelTime + this.hatchery_construction_time);
+        }
+        this.minerals += optimalMinerals + suboptimalMinerals;
     }
 
     addOverlord() {
@@ -51,5 +89,21 @@ export default class Game {
         this.max_supply = this.overlord_supply_increase;
 
         this.minerals = this.overlord_construction_time * this.drone_count * this.mining_rate;
+    }
+
+    getDroneCount() {
+        return this.drone_count;
+    }
+
+    getHatcheryCount() {
+        return this.hatchery_count;
+    }
+
+    getLarvaCount() {
+        return this.currentLarvaCount;
+    }
+
+    getMineralCount() {
+        return Math.floor(this.minerals);
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,42 +2,73 @@ import Game from './game.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const canvas = document.getElementById('canvas');
-    const canvasEl = canvas[0]
+    //const canvasEl = canvas[0]
     const ctx = canvas.getContext('2d');
-    canvas.width = 800;
-    canvas.height = 400;
+    canvas.width = 830;
+    canvas.height = 559;
+    ctx.font = '36px serif';
+    ctx.fillStyle = '#eaeaea';
+    const game = new Game();
+
+    const map = new Image();
+    map.src = '../src/atmospheres.jpg';
+    map.addEventListener('load', function () {
+        ctx.drawImage(map, 0, 0, 600, 559);
+    })
 
     const drone = new Image();
     drone.src = '../src/drone.jpeg';
     drone.addEventListener('load', function() {
-        
-        ctx.drawImage(drone, 0, 0, 57, 56);
+        ctx.drawImage(drone, canvas.width - 230, 100, 57, 56);
     })
     
     const hatchery = new Image();
     hatchery.src = '../src/hatchery.jpg';
     hatchery.addEventListener('load', function () {
-        ctx.drawImage(hatchery, 75, 0, 76, 56);
+        ctx.drawImage(hatchery, canvas.width - 165, 100, 66, 56);
     })
 
-    const game = new Game();
+    const minerals = new Image();
+    minerals.src = '../src/minerals.jpeg';
+    minerals.addEventListener('load', function () {
+        ctx.drawImage(minerals, canvas.width - 100, 100, 56, 56);
+    })
 
-});
+    ctx.fillText(game.displayTime(), canvas.width -  50, 0, 100);
+    ctx.fillText(game.getDroneCount(), 0, 200, 100);
+    ctx.fillText(game.getHatcheryCount(), 76, 200, 100);
+    ctx.fillText(game.getMineralCount(), 76 + 75 + 5, 200, 100);
 
-const ui = document.getElementById('canvas');
-
-ui.addEventListener('click', function (e) {
-    if (e.pageX >= ui.offsetLeft + ui.clientLeft &&
-        e.pageX <= ui.offsetLeft + ui.clientLeft + 57 &&
-        e.pageY >= ui.offsetTop + ui.clientTop &&
-        e.pageY <= ui.offsetTop + ui.clientTop + 56) {
-            game.addDrone();
+    canvas.addEventListener('click', function (e) {
+        // Drone
+        if (e.pageX >= canvas.offsetLeft + canvas.clientLeft &&
+            e.pageX <= canvas.offsetLeft + canvas.clientLeft + 57 &&
+            e.pageY >= canvas.offsetTop + canvas.clientTop + 100 &&
+            e.pageY <= canvas.offsetTop + canvas.clientTop + 100 + 56) {
+                game.addDrone();
+                ctx.clearRect(0, 167, 67, 40);
+                ctx.fillText(game.getDroneCount(), 0, 200, 100);
+                ctx.clearRect(0, 0, 100, 100);
+                ctx.fillText(game.displayTime(), 0, 50, 100);
+                ctx.clearRect(76 + 75 + 5, 167, 100, 40);
+                ctx.fillText(game.getMineralCount(), 76 + 75 + 5, 200, 100);
         }
 
-    if (e.pageX >= ui.offsetLeft + ui.clientLeft + 75 &&
-        e.pageX <= ui.offsetLeft + ui.clientLeft + 75 + 76 &&
-        e.pageY >= ui.offsetTop + ui.clientTop &&
-        e.pageY <= ui.offsetTop + ui.clientTop + 56) {
-        game.addHatchery();
-    }
+        // Hatchery
+        if (e.pageX >= canvas.offsetLeft + canvas.clientLeft + 75 &&
+            e.pageX <= canvas.offsetLeft + canvas.clientLeft + 75 + 76 &&
+            e.pageY >= canvas.offsetTop + canvas.clientTop + 100 &&
+            e.pageY <= canvas.offsetTop + canvas.clientTop + 156) {
+                game.addHatchery();
+                ctx.clearRect(75, 167, 76, 40);
+                ctx.fillText(game.getHatcheryCount(), 76, 200, 100);
+                ctx.clearRect(0, 0, 100, 100);
+                ctx.fillText(game.displayTime(), 0, 50, 100);
+                ctx.clearRect(76 + 75 + 5, 167, 100, 40);
+                ctx.fillText(game.getMineralCount(), 76 + 75 + 5, 200, 200);
+                ctx.clearRect(0, 167, 67, 40);
+                ctx.fillText(game.getDroneCount(), 0, 200, 100);
+        }
+    });
+
 });
